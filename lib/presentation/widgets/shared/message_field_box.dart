97766f521b6a4b9978b6fd82d51229a656b92467
SergioCamiloCastillo <scastillonu@uniminuto.edu.co>
2023-04-06 20:55:30 -0500

    diseño chat

---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final FocusNode focusNode = FocusNode();
+
+    final outlineInputBorder = UnderlineInputBorder(
+        borderSide: const BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(20));
+
+    final inputDecoration = InputDecoration(
+        hintText: "Termina tu mensaje con '?'",
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send_outlined),
+          onPressed: () {
+            final textValue = textController.value.text;
+            textController.clear();
+            print("button=>$textValue");
+          },
+        ));
+
+    return TextFormField(
+      onTapOutside: (event){
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print("submit value ${value}");
+        textController.clear();
+        focusNode.requestFocus();
+      },
+    );
+  }
+}
